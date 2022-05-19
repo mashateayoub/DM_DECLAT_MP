7c445bececbc5380c4ab37e96998e197ac7b651a
Mashate Ayoub <68016682+mashateayoub@users.noreply.github.com>
2022-05-19 13:32:35 +0100

    plots again an tests

--- Main.py
+++ Main.py
@@ -7,28 +7,43 @@ import tracemalloc
 from pyECLAT import ECLAT
 import pandas as pd
 import matplotlib.pyplot as plt
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
-# dEclat  : minsupp=20%
-# Test :
-print("==================== dEclat: (min_comb=1 & max_comb=2) ==========================")
+# # dEclat  : minsupp=20%
+# # Test :
+# print("==================== dEclat: (max_comb=2) ==========================")
 
-d = dEclat(minsupp=20, data="diabetes.csv")
-d.rundEclat()
-
-
-print(" ")
-print("=========================== Statistiques : ============================")
-print("Minimum support: "+str(d.minsupp)+"%")
-file_size = os.path.getsize(r'diabetes.csv') 
-print("Database Size: ", file_size/1024, "Kb")
-print("Transactions count from database :"+str(len(d.data)))
-print("Frequent itmsets count :"+str(len(d.frequentItems)))
-print("Total Time ~ "+str((d.end_timestamp - d.start_timestamp))+" s")
-print("Maximum Memory usage ~ "+str(d.peak_memory/1024 )+" Kb")
+# d = dEclat(minsupp=20, data="diabetes.csv")
+# d.rundEclat()
 
 
+# print(" ")
+# print("=========================== Statistiques : ============================")
+# print("Minimum support: "+str(d.minsupp)+"%")
+# file_size = os.path.getsize(r'diabetes.csv') 
+# print("Database Size: ", file_size/1024, "Kb")
+# print("Transactions count from database :"+str(len(d.data)))
+# print("Frequent itmsets count :"+str(len(d.frequentItems)))
+# print("Total Time ~ "+str((d.end_timestamp - d.start_timestamp))+" s")
+# print("Maximum Memory usage ~ "+str(d.peak_memory/1024 )+" Kb")
 
-#Experience : 
+# # ECLAT 
+# dataframe = pd.read_csv('DiabetesTransactions.csv', header=None) 
+# eclat_instance = ECLAT(data=dataframe, verbose=True)
+
+# eclat_instance.df_bin   #generate a binary dataframe, that can be used for other analyzes.
+# eclat_instance.uniq_    #a list with all the names of the different items
+# get_ECLAT_indexes, get_ECLAT_supports = eclat_instance.fit(min_support=0.2,
+#                                                             min_combination=1,
+#                                                             max_combination=2,
+#                                                             separator=' , ',
+#                                                             verbose=True)
+# print(get_ECLAT_supports)
+
+
+
+# Experience : 
 
 # DECLAT
 print(" ")
@@ -53,6 +68,7 @@ eclat_instance = ECLAT(data=dataframe, verbose=True)
 eclat_instance.df_bin   #generate a binary dataframe, that can be used for other analyzes.
 eclat_instance.uniq_    #a list with all the names of the different items
 
+
 MinSupps=[]
 Times=[]
 MemUsage=[]
@@ -63,7 +79,7 @@ for i in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]:
     get_ECLAT_indexes, get_ECLAT_supports = eclat_instance.fit(min_support=i,
                                                             min_combination=1,
                                                             max_combination=2,
-                                                            separator=' & ',
+                                                            separator=' , ',
                                                             verbose=True)
     
     _,peak_memory = tracemalloc.get_traced_memory()
@@ -74,12 +90,33 @@ for i in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]:
     Times.append(end_timestamp - start_timestamp)
     MemUsage.append(peak_memory/1024)
 
+# Apriori
+columns=[ "NormalGlucose",  "PrediabeteGlucose", "DiabeteGlucose","NormalBloodPressure", "Hypertension1BloodPressure", "Hypertension2BloodPressure" , "UnderweightBMI", "HealthyweightBMI", "OverweightBMI", "ObesityBMI","Childhood",  "Adolescence","EarlyAdulthood", "Adulthood","MiddleAge", "EarlyElder", "LateElder", "OutcomeYes", "OutcomeNo"]
+dataframe = pd.read_csv('BinaryTransactions.csv', header=None)
+MinSuppsA=[]
+TimesA=[]
+MemUsageA=[]
+FreqItemsA=[]
+for i in [0.2, 0.3, 0.4, 0.5, 0.6, 0.7]:
+    start_timestamp = time.time()
+    tracemalloc.start()
+    frequent_itemsets = apriori(dataframe, min_support=i, use_colnames=True, max_len=2).sort_values(by=['support'], ascending=False)
+    _,peak_memory = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    end_timestamp = time.time()
+    FreqItemsA.append(len(frequent_itemsets))
+    MinSuppsA.append(i*100)
+    TimesA.append(end_timestamp - start_timestamp)
+    MemUsageA.append(peak_memory/1024)
+
+
 
 
 # Scatter Plot : Memory usage  / min supports
 fig, ax = plt.subplots()
 ax.plot(np.array(MinSuppsD), np.array(MemUsageD),label="diffeclat")
-ax.plot(np.array(MinSupps), np.array(MemUsage), label="eclat", linestyle='dashed')
+ax.plot(np.array(MinSupps), np.array(MemUsage), label="eclat")
+ax.plot(np.array(MinSuppsA), np.array(MemUsageA),label="MLxtend-Apriori", color="red")
 
 ax.set(xlabel='Minimum Supports (%)', ylabel='Memory usage (Kb)',
        title='Memory usage variation in function of min supports ')
@@ -92,8 +129,10 @@ plt.show()
 
 # Scatter Plot : freq items number / min supports
 fig, ax = plt.subplots()
-ax.scatter(np.array(MinSupps), np.array(FreqItems) , marker = '*', color = 'green')
+ax.scatter(np.array(MinSuppsA), np.array(FreqItemsA), marker = 'o', color = 'blue')
+ax.scatter(np.array(MinSupps), np.array(FreqItems) , marker = '*', color = 'yellow')
 ax.scatter(np.array(MinSuppsD), np.array(FreqItemsD) , marker = '+', color = 'red')
+
 
 ax.set(xlabel='Minimum Supports (%)', ylabel='Frequent items number',
        title='Freq items variation in function of min supports ')
